8ecbbf3d603fb25b1817d370f3af777f5bb25ea5
raj129 <raj@collagia.com>
2023-05-15 08:39:36 +0530

    Removed wait time

--- tap_nike/client.py
+++ tap_nike/client.py
@@ -64,7 +64,6 @@ class nikeStream(RESTStream):
         if response.json()["pages"]["next"] == "":
             return None
         self.path = response.json()["pages"]["next"]
-        time.sleep(120)
         logging.info(f"$$$$$$$ pages updated here {self.path}")
         return response.json()["pages"]["next"]
 
