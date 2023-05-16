3dd78da3cc67ff2f80edc89844b9a05182c41b5c
raj129 <raj@collagia.com>
2023-05-16 09:11:21 +0530

    Used nex_token anchor value as replication_key

--- tap_nike/client.py
+++ tap_nike/client.py
@@ -3,13 +3,7 @@
 from __future__ import annotations
 
 import logging
-import time
-
-import boto3
-from botocore.exceptions import ClientError
-import os
 from pathlib import Path
-import pickle
 from typing import Any, Callable, Iterable
 
 import requests
@@ -25,29 +19,8 @@ class nikeStream(RESTStream):
 
     # TODO: Set the API's base URL here:
     url_base = "https://api.nike.com/"
-
     records_jsonpath = "$[*]"  # Or override `parse_response`.
     next_page_token = ""  # Or override `get_next_page_token`
-    file_key = "default_file_key"
-    bucket_name = 'meltano-state'
-    custom_state = {}
-
-    def initialise_state(self):
-        s3 = boto3.client('s3')
-        logging.info("CONFIG JOB ID OBTAINED")
-        logging.info(self.config["job_id"])
-        self.file_key = self.config["job_id"]
-        logging.info(f"######### FOLLOWING file key obtained {self.file_key}")
-        try:
-            s3.head_object(Bucket=self.bucket_name, Key=self.file_key)
-        except ClientError as e:
-            logging.info(f"S3 init Exception occurred")
-            traceback.print_exc()
-        else:
-            # Load the object from S3 using pickle
-            resp = s3.get_object(Bucket= self.bucket_name, Key=self.file_key)
-            data = resp['Body'].read()
-            self.custom_state = pickle.loads(data)
 
     @property
     def http_headers(self) -> dict:
@@ -67,6 +40,11 @@ class nikeStream(RESTStream):
 
         return headers
 
+    def get_url(self, response: requests.Response) -> str:
+        if self.tap_state["bookmarks"][self.name].get('replication_key_value'):
+            self.path = self.tap_state["bookmarks"][self.name].get('replication_key_value')
+        return "".join([self.url_base, self.path or ""])
+
     def get_next_page_token(
         self,
         response: requests.Response,
@@ -81,41 +59,14 @@ class nikeStream(RESTStream):
         Returns:
             The next pagination token.
         """
-        if response.json()["pages"]["prev"] == "":
-            # first url, setting up the state.
-            self.initialise_state()
-        if response.json()["pages"]["next"] == "":
-            #last url, wriitng state to s3
-            try:
-                s3 = boto3.client('s3')
-                s3.put_object(Bucket=self.bucket_name, Key=self.file_key, Body=pickle.dumps(self.custom_state))
-            except Exception as e:
-                logging.info(f"S3 put exception")
-                traceback.print_exc()
-            return None
+        if self.path == response.json()["pages"]["next"]:
+            return ""
+        logging.info(f"TAP STATE {self.tap_state}")
+        next_token = response.json()["pages"]["next"]
+        logging.info(f"$$$$$$$ pages updated here {self.path} next token {next_token}")
         self.path = response.json()["pages"]["next"]
-        logging.info(response.json()["pages"]["prev"])
-        logging.info(f"$$$$$$$ pages updated here {self.path}")
         return response.json()["pages"]["next"]
 
-    # def get_url_params(
-    #     self,
-    #     context: dict | None,
-    #     next_page_token: Any | None,
-    # ) -> dict[str, Any]:
-    #     """Return a dictionary of values to be used in URL parameterization.
-    #
-    #     Args:
-    #         context: The stream context.
-    #         next_page_token: The next page index or value.
-    #
-    #     Returns:
-    #         A dictionary of URL query parameters.
-    #     """
-    #     params: dict = {}
-    #     channel_id = self.config["channel_id"]
-    #     params["filter"] = f'language(en),marketplace(US),channelId({channel_id}),inStock(false),includeExpired(true)'
-    #     return params
 
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
         """Parse the response and return an iterator of result records.
@@ -134,6 +85,7 @@ class nikeStream(RESTStream):
                         for k, v in product_info.items():
                             if k == "merchProduct":
                                 try:
+                                    flatten_dict["next_token"] = response.json()["pages"]["next"]
                                     flatten_dict["merchGroup"] = product_info["merchProduct"]["merchGroup"]
                                     flatten_dict["styleCode"] = product_info["merchProduct"]["styleCode"]
                                     flatten_dict["styleColor"] = product_info["merchProduct"]["styleColor"]
@@ -168,13 +120,7 @@ class nikeStream(RESTStream):
                                                             for k, v in n_node["properties"]["squarish"].items():
                                                                 if k == "url":
                                                                     flatten_dict["ITEM_IDENTIFIER"] = n_node["properties"]["squarish"]["url"]
-                                                        if not self.custom_state.get("identifiers"):
-                                                            self.custom_state["identifiers"] = [flatten_dict["ITEM_IDENTIFIER"]]
-                                                            yield flatten_dict
-                                                        elif flatten_dict["ITEM_IDENTIFIER"] not in \
-                                                                    self.custom_state["identifiers"]:
-                                                            self.custom_state["identifiers"].append(flatten_dict["ITEM_IDENTIFIER"])
-                                                            yield flatten_dict
+                                                        yield flatten_dict
                                                     except Exception as e:
                                                         traceback.print_exc()
                                 except Exception as e:
--- tap_nike/streams.py
+++ tap_nike/streams.py
@@ -20,7 +20,7 @@ class NikeStream(nikeStream):
     name = "nike"
     path = "product_feed/threads/v2?filter=language%28en%29&filter=marketplace%28US%29&filter=channelId%28d9a5bc42-4b9c-4976-858a-f159cf99c647%29&filter=inStock%28false%29&filter=includeExpired%28true%29"
     primary_keys = "ITEM_IDENTIFIER"
-    replication_key = "modificationDate"
+    replication_key = "next_token"
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     schema = th.PropertiesList(
         th.Property(
@@ -76,6 +76,10 @@ class NikeStream(nikeStream):
         th.Property(
             "currentPrice",
             th.NumberType,
+        ),
+        th.Property(
+            "next_token",
+            th.StringType
         )
     ).to_dict()
 
@@ -85,7 +89,7 @@ class SnkrsStream(nikeStream):
     name = "Snkrs"
     path = "product_feed/threads/v2?filter=language%28en%29&filter=marketplace%28US%29&filter=channelId%28010794e5-35fe-4e32-aaff-cd2c74f89d61%29&filter=inStock%28false%29&filter=includeExpired%28true%29"
     primary_keys = "ITEM_IDENTIFIER"
-    replication_key = "modificationDate"
+    replication_key = "next_token"
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     schema = th.PropertiesList(
         th.Property(
@@ -145,6 +149,10 @@ class SnkrsStream(nikeStream):
         th.Property(
             "currentPrice",
             th.NumberType,
+        ),
+        th.Property(
+            "next_token",
+            th.StringType
         )
     ).to_dict()
 
@@ -155,7 +163,7 @@ class NikeMobileStream(nikeStream):
     name = "nike_mobile"
     path = "product_feed/threads/v2?filter=language%28en%29&filter=marketplace%28US%29&filter=channelId%2882a74ac1-c527-4470-b7b0-fb5f3ef3c2e2%29&filter=inStock%28false%29&filter=includeExpired%28true%29"
     primary_keys = "ITEM_IDENTIFIER"
-    replication_key = "modificationDate"
+    replication_key = "next_token"
     # Optionally, you may also use `schema_filepath` in place of `schema`:
     schema = th.PropertiesList(
         th.Property(
@@ -215,5 +223,9 @@ class NikeMobileStream(nikeStream):
         th.Property(
             "currentPrice",
             th.NumberType,
+        ),
+        th.Property(
+            "next_token",
+            th.StringType
         )
     ).to_dict()
